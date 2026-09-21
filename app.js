b13cbde46fce304d24d2446b3beca4fae7ce60c8
Bilal-Kasiri <muhammadbilalkasiri@gmail.com>
2026-09-21 10:58:16 +0500

    Fix port for deployment

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ app.post("/orders", async (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
